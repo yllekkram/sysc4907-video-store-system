e61e64d01f7c84d0daf745b773e2fbc60f2247a2
LaFamiglia <LaFamiglia@CasaMain.gateway.2wire.net>
2013-01-06 14:43:13 -0500

    Fixed the 404 error for the login increment

--- VSS-web/src/main/java/com/team33/controllers/LoginController.java
+++ VSS-web/src/main/java/com/team33/controllers/LoginController.java
@@ -6,7 +6,9 @@ package com.team33.controllers;
 
 import com.team33.entities.Account;
 import com.team33.services.AccountService;
-import com.team33.services.AuthenticationException;
+import com.team33.services.exception.AccountNotActivatedException;
+import com.team33.services.exception.AccountNotFoundException;
+import com.team33.services.exception.AuthenticationException;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,28 +24,21 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
-    
-    public LoginController() {
-        //Initialize controller properties here or 
-        //in the Web Application Context
-
-        //setCommandClass(MyCommand.class);
-        //setCommandName("MyCommandName");
-        //setSuccessView("successView");
-        //setFormView("formView");
-    }
-    
      public static final String ACCOUNT_ATTRIBUTE = "account";
 
     @Autowired
     private AccountService accountService;
+    
+    public void setAccountService(AccountService service) {
+        this.accountService = service;
+    }
     
     @RequestMapping(method = RequestMethod.GET)
     public String login() {
         return "login";
     }
     
-     @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public String handleLogin(@RequestParam String username, 
     @RequestParam String password, RedirectAttributes redirect,HttpSession session)
     throws AuthenticationException {
@@ -52,6 +47,12 @@ public class LoginController {
              session.setAttribute(ACCOUNT_ATTRIBUTE, account);
              return "redirect:/index.htm";
          } catch (AuthenticationException ae) {
+             redirect.addFlashAttribute("exception", ae);
+             return "redirect:/login";
+         } catch (AccountNotActivatedException ae) {
+             redirect.addFlashAttribute("exception", ae);
+             return "redirect:/login";
+         } catch (AccountNotFoundException ae) {
              redirect.addFlashAttribute("exception", ae);
              return "redirect:/login";
          }
--- VSS-web/src/main/java/com/team33/services/AccountServiceImpl.java
+++ VSS-web/src/main/java/com/team33/services/AccountServiceImpl.java
@@ -4,8 +4,11 @@
  */
 package com.team33.services;
 
+import com.team33.services.exception.AuthenticationException;
 import com.team33.entities.Account;
 import com.team33.entities.dao.AccountDao;
+import com.team33.services.exception.AccountNotActivatedException;
+import com.team33.services.exception.AccountNotFoundException;
 import java.util.List;
 import org.springframework.dao.DataAccessException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +22,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    @Transactional
-    public Account loginAccount(String Username, String Password) throws AuthenticationException {         
-        return accountDao.getAccount(Username);
-    }
-
-    
-
     @Autowired
     private AccountDao accountDao;
     
+    public void setAccountDAO(AccountDao dao) {
+        this.accountDao = dao;
+    }
+
+
+    @Transactional
+    public Account loginAccount(String username, String password) throws AuthenticationException, AccountNotFoundException, AccountNotActivatedException { 
+        Account account = accountDao.getAccount(username);
+        if (account == null)
+            throw new AccountNotFoundException("Account was not found");
+        if (account.getActivated() != Boolean.TRUE) {
+            throw new AccountNotActivatedException("Account not activated!");
+        } else {
+            if (account.getName().matches(username))
+                if (!account.getPassword().matches(password))
+                    throw new AuthenticationException("Unable to authenticate the account");
+        }
+        return account;
+    }
+
     @Transactional
     @Override
     public void registerAccount(Account account) throws DataAccessException {
