8409efe9f4939b7be783111f4ea0ab2e3fcabf2a
Caleb <Caleb@Caleb-PC>
2013-01-07 00:21:17 -0500

    ** Note ** 	The submit button is broken in registerAccountView.jsp and will return a 500 error when adding the account to the db. This will be fixed in a later commit.
    AccountController.java
    	- Added debug code
    	- Modified request mapping of controller to match registerAccountController.jsp
    	- Removed extends as its not being used currently
    
    AccountDaoImpl.java
    	- Changed getAccounts() method to correctly access Account table
    
    account.hbm.xml
    	- Added meta data about Account table for hibernate to map
    
    spring-hibernate.xml
    	- Added account.hbm.xml to list of resources for hibernate
    	- Injected dependencies for service and dao classes
    
    dispatcher-servlet.xml
    	- Removed duplicate instance of org.springframework.web.servlet.mvc.support.ControllerClassNameHandlerMapping
    	- Mapped pages used by registerAccountView.jsp to url map
    
    registerAccountView.jsp
    	- Fixed form to map the saveAccount() method and getAccounts() method to correct functionality
    	** Note **
    		The submit button is broken in registerAccountView.jsp and will return a 500 error when adding the account to the db. This will be fixed in a later commit.
    	
    index.jsp
    	- Added link to navigate to registerAccountView.htm

--- VSS-web/src/main/java/com/team33/entities/dao/AccountDaoImpl.java
+++ VSS-web/src/main/java/com/team33/entities/dao/AccountDaoImpl.java
@@ -1,8 +1,7 @@
 package com.team33.entities.dao;
 
-import java.util.List;
-
 import com.team33.entities.Account;
+import java.util.List;
 import org.hibernate.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -15,16 +14,18 @@ public class AccountDaoImpl implements AccountDao {
         @Autowired
         private SessionFactory sessionFactory ;
         
+        public void setSessionFactory(SessionFactory sessionFactory){
+            this.sessionFactory = sessionFactory;
+        }
+        
 	@Override
 	public List<Account> getAccounts() throws DataAccessException {
-		return sessionFactory.getCurrentSession().createQuery("active"
-                        + " Account").list();
+		return sessionFactory.getCurrentSession().createQuery("from" + " Account").list();
 	}
 
 	@Override
 	public Account getAccount(Long accountId) throws DataAccessException {
-		return (Account)sessionFactory.getCurrentSession().get(Account.class, 
-                        accountId);
+		return (Account)sessionFactory.getCurrentSession().get(Account.class, accountId);
 	}
         
         @Override
@@ -39,8 +40,7 @@ public class AccountDaoImpl implements AccountDao {
 
 	@Override
 	public void saveAccount(Account account) throws DataAccessException {
-		sessionFactory.getCurrentSession().save(account);
-		
+		sessionFactory.getCurrentSession().save(account);	
 	}
         
         @Override
