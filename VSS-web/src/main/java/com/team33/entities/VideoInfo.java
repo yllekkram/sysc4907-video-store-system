f5763a6dcfeb04f594fd93d008f77084c7e4f7bc
Mark Kelly <markkelly0808@gmail.com>
2013-01-09 14:57:12 -0500

    implemented VideoInfoDao interface

--- VSS-web/src/main/java/com/team33/entities/VideoInfo.java
+++ VSS-web/src/main/java/com/team33/entities/VideoInfo.java
@@ -11,6 +11,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -23,6 +25,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "Video_Info")
 @XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "VideoInfo.findAll", query = "SELECT v FROM VideoInfo v")
+})
 public class VideoInfo implements Serializable {
     private static final long serialVersionUID = 1L;
     
