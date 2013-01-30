e6dec3d2b43dae7dfafe5fe83272e43ab2ebb755
Caleb <Caleb@Caleb-PC>
2013-01-29 21:51:20 -0500

    - Rewrote the test classes for AccountDaoImpl and VideoInfoDao - Added generated test class for BrowseDaoImpl

---
+++ VSS-web/src/test/java/com/team33/entities/dao/BrowseDaoImplTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team33.entities.dao;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Caleb
+ */
+public class BrowseDaoImplTest {
+    
+    public BrowseDaoImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of displayFeaturedVideo method, of class BrowseDaoImpl.
+     */
+    @Test
+    public void testDisplayFeaturedVideo() {
+        System.out.println("displayFeaturedVideo");
+        BrowseDaoImpl instance = new BrowseDaoImpl();
+        instance.displayFeaturedVideo();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
