71e0fd8c04b8de535a50c1281dd8215ccfed1c79
Caleb <Caleb@Caleb-PC>
2013-02-04 00:41:38 -0500

    - Added new test classes for new classes added

--- VSS-web/src/test/java/com/team33/entities/dao/BrowseDaoImplTest.java
+++ VSS-web/src/test/java/com/team33/entities/dao/BrowseDaoImplTest.java
@@ -4,18 +4,29 @@
  */
 package com.team33.entities.dao;
 
+import com.team33.entities.ScreenRating;
+import com.team33.entities.VideoInfo;
+import com.team33.services.exception.DataAccessException;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
  * @author Caleb
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/applicationContext-test.xml"})
 public class BrowseDaoImplTest {
+    
+    private BrowseDaoImpl browseDao;
     
     public BrowseDaoImplTest() {
     }
@@ -35,16 +46,86 @@ public class BrowseDaoImplTest {
     @After
     public void tearDown() {
     }
+    
+    public void setBrowseDao(BrowseDaoImpl browseDao){
+        this.browseDao = browseDao;
+    }
 
     /**
      * Test of displayFeaturedVideo method, of class BrowseDaoImpl.
      */
     @Test
     public void testDisplayFeaturedVideo() {
-        System.out.println("displayFeaturedVideo");
-        BrowseDaoImpl instance = new BrowseDaoImpl();
-        instance.displayFeaturedVideo();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("displayFeaturedVideo() : List<VideoInfo>");
+        
+        List<VideoInfo> videos = this.browseDao.displayFeaturedVideo();
+        assertNotNull(videos);
+        
+        assertTrue(videos.size() > 0);
+    }
+    
+    @Test
+    public void testSearchVideos_ScreenRating(){
+        System.out.println("searchVideos(ScreenRating) : List<VideoInfo>");
+
+        try{
+            List<VideoInfo> videos = this.browseDao.searchVideos((ScreenRating)null);
+            fail("Exception was not thrown");
+        }catch(DataAccessException e){
+        
+        }
+        ScreenRating testRating = new ScreenRating();
+        testRating.setId(9999);
+        List<VideoInfo> videos = this.browseDao.searchVideos(testRating);
+        
+        assertNotNull(videos);
+        assertEquals(videos.size(), 0);
+        
+        testRating = new ScreenRating();
+        testRating.setId(0);
+        videos = this.browseDao.searchVideos(testRating);
+        
+        assertNotNull(videos);
+        assertTrue(videos.size() > 0);
+        
+        // TODO Need to check if correct videos appear
+    }
+    
+    @Test
+    public void testSearchVideos_String(){
+        System.out.println("searchVideos(String) : List<VideoInfo>");
+        
+        try{
+            List<VideoInfo> videos = this.browseDao.searchVideos((String)null);
+            fail("Exception was not thrown");
+        }catch(DataAccessException e){
+        
+        }
+        
+        List<VideoInfo> videos = this.browseDao.searchVideos("");
+        assertNotNull(videos);
+        assertEquals(videos.size(), 0);
+        
+        videos = this.browseDao.searchVideos("No Going to Find Something");
+        assertNotNull(videos);
+        assertEquals(videos.size(), 0);
+        
+        videos = this.browseDao.searchVideos("Single Title");
+        assertNotNull(videos);
+        assertEquals(videos.size(), 1);
+        VideoInfo testVideoInfo = videos.get(0);
+        
+        assertNotNull(testVideoInfo);
+        
+        // TODO test for video retrieved
+        
+        videos = this.browseDao.searchVideos("Multiple Title");
+        assertNotNull(videos);
+        assertEquals(videos.size(), 2);
+        
+        assertNotNull(videos.get(0));
+        assertNotNull(videos.get(1));
+        
+        // TODO test for videos retrieved
     }
 }
