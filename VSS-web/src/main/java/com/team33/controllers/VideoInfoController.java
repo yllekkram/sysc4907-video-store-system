8b8620aea5f5d099979348ffa76b81f7f939f068
Mark Kelly <markkelly0808@gmail.com>
2013-02-04 10:06:43 -0500

    add dynamic sections to browseVideos.jsp

--- VSS-web/src/main/java/com/team33/controllers/VideoInfoController.java
+++ VSS-web/src/main/java/com/team33/controllers/VideoInfoController.java
@@ -4,6 +4,8 @@
  */
 package com.team33.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class VideoInfoController {
     @RequestMapping(value = "/browseVideos", method = RequestMethod.GET)
     public String getVideos(Map<String,Object> map) {
+        List<Object> list = new ArrayList<Object>();
+        list.add(new DummyVideoInfo("The Lion King", "Mufasa dies.", "G", "Family"));
+        list.add(new DummyVideoInfo("Top Gun", "Goose dies.", "PG-13", "Action"));
+        list.add(new DummyVideoInfo("Final Destination", "They all die.", "14A", "Horror"));
+        map.put("videoInfoList", list);
         return "/browseVideos";
+    }
+    
+    public class DummyVideoInfo {
+        String name;
+        String description;
+        String screenRating;
+        String genre;
+        
+        public DummyVideoInfo(String name, String description, String screenRating, String genre) {
+            this.name = name;
+            this.description = description;
+            this.screenRating = screenRating;
+            this.genre = genre;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getScreenRating() {
+            return screenRating;
+        }
+
+        public String getGenre() {
+            return genre;
+        }
     }
 }
--- VSS-web/src/main/webapp/WEB-INF/views/browseVideos.jsp
+++ VSS-web/src/main/webapp/WEB-INF/views/browseVideos.jsp
@@ -4,14 +4,38 @@
     Author     : Mark
 --%>
 
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Videos</title>
+        
+        <style>
+            th.videoListHeader {
+                text-align:left;
+            }
+        </style>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Browse Videos</h1>
+        <table>
+            <tr>
+                <th class="videoListHeader">Name</th>
+                <th class="videoListHeader">Description</th>
+                <th class="videoListHeader">Rating</th>
+                <th class="videoListHeader">Genre</th>
+            </tr>
+            <c:forEach var="videoInfo" items="${videoInfoList}">
+                <tr>
+                    <td><c:out value="${videoInfo.getName()}"/></td>
+                    <td><c:out value="${videoInfo.getDescription()}"/></td>
+                    <td><c:out value="${videoInfo.getScreenRating()}"/></td>
+                    <td><c:out value="${videoInfo.getGenre()}"/></td>
+                </tr>
+            </c:forEach>
+        </table>
     </body>
 </html>
