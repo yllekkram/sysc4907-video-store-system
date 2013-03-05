29aa973c3cd9bbe590a1c9c9d3814b7bc53e2251
Mark Kelly <markkelly0808@gmail.com>
2013-03-05 00:03:58 -0500

    remove browseVideos mapping entry from dispatcher-servlet since it is redundant with annotations

--- VSS-web/src/main/java/com/team33/controllers/BrowseVideosController.java
+++ VSS-web/src/main/java/com/team33/controllers/BrowseVideosController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
