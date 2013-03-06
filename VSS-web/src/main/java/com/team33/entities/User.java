3989f6d3747306e0634278c6a945f681dc3d4ebc
Mark Kelly <markkelly0808@gmail.com>
2013-03-05 22:57:30 -0500

    create User entity

---
+++ VSS-web/src/main/java/com/team33/entities/User.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team33.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Mark
+ */
+
+@Entity
+@Table(name="USERS")
+public class User {
+    @Id
+    @Column(name="ID")
+    @GeneratedValue
+    private Integer id;
+    
+    @Column(name="NAME")
+    private String name;
+    
+    @Column(name="EMAIL")
+    private String email;
+    
+    @Column(name="PASSWORDHASH")
+    private String passwordHash;
+    
+    @Column(name="ACTIVATED")
+    private Boolean activated;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public void setActivated(Boolean activated) {
+        this.activated = activated;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public Boolean getActivated() {
+        return activated;
+    }
+}
